431f3f793ed1efa6eb89e493acc4ad4706b532a3
a.evladov <eaa67@mail.ru>
2023-03-20 16:16:47 +0300

    Заносим данные в таблицу Показываем данные на экране

--- app/src/main/java/com/example/dataroom/MainActivity.kt
+++ app/src/main/java/com/example/dataroom/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dataroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.asLiveData
 import com.example.dataroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
+        val db = MainDB.getDB(this)
+
+        db.getDao().getAllItem().asLiveData().observe(this) { list ->
+            binding.edMessage.text = ""
+            list.forEach {
+                val text = "Id: ${it.id} Name: ${it.name} Price: ${it.price}\n"
+                binding.edMessage.append(text)
+            }
+        }
+
+        binding.btnSave.setOnClickListener {
+            val item = Item(
+                null,
+                binding.edName.text.toString(),
+                binding.edPrice.text.toString(),
+            )
+
+            Thread {
+                db.getDao().insertItem(item)
+            }.start()
+        }
+    }
 }
